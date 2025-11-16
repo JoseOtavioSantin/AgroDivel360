d9f3a6636a164c94f4b883d152e0ba33ea156ad9
JoseOtavioSantin <57141195+JoseOtavioSantin@users.noreply.github.com>
2025-11-15 23:57:35 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,26 +1,20 @@
-// /assets/js/firebase-config.js (Versão completa e recomendada)
-
-// Importe as funções que você vai precisar do SDK do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js"; // Adicionado
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js";
 
-// Sua configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDcjPa9jXsCCu6lNc1fjVg4Bzz1toKWAGY",
   authDomain: "agro-divel.firebaseapp.com",
   projectId: "agro-divel",
-  storageBucket: "agro-divel.appspot.com",
+  storageBucket: "agro-divel.firebasestorage.app", 
   messagingSenderId: "583977436505",
   appId: "1:583977436505:web:3754ec029aebb3d9d67848"
 };
 
-// Inicializa o Firebase e cria as instâncias
 const app = initializeApp(firebaseConfig );
 const db = getFirestore(app);
 const auth = getAuth(app);
-const storage = getStorage(app); // Adicionado
+const storage = getStorage(app);
 
-// Exporta as instâncias e funções para usar em outros arquivos
-export { db, auth, storage, onAuthStateChanged, signOut, doc, getDoc }; // Adicionado 'storage'
+export { db, auth, storage, onAuthStateChanged, signOut, doc, getDoc };
